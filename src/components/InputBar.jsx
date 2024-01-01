1a78e157398ed21216b058532de10762b1ea9284
Nathan George <georgenathan010@gmail.com>
2024-01-01 20:04:10 +0300

    I decided to use a different approach because I had too many state variables

--- src/components/InputBar.jsx
+++ src/components/InputBar.jsx
@@ -1,49 +1,73 @@
-import React, {useState} from "react";
-import { FaRegPlusSquare } from "react-icons/fa";
-import Task from "./task";
-
+import React,{useState} from 'react'
+import {FaRegPlusSquare, FaTrashAlt} from 'react-icons/fa'
 
 
 const InputBar = () => {
-  const [role, setRole] = useState('go shopping');
+  const [textValue, setTextValue] = useState('')
+  const [tasks, setTasks] = useState([])
+  const handleTextValue = (e) => {
+    setTextValue(e.target.value)
+  }
 
   const handleClick = () => {
-    console.log('helo')
+      if(textValue.trim() !== '') {
+        setTasks([...tasks, textValue])
+        setTextValue('')
+      }
   }
-  const handleForm = (e) => {
-    setRole(e.target.value)
-    
+  const handleDelete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index,1);
+    setTasks(updatedTasks)
   }
+ 
   return (
     <>
-      <section
-      className="border-2 border-black bg-red-200 flex" 
-      >
-        <input 
-        type="text" 
-        className="border-2
-         border-black p- "
-         value = {role} 
-         onChange={handleForm}
-        />
-        <FaRegPlusSquare 
-        role = 'button'
-        size={40}
-        style = {{
+    <section className='flex'>
+    <input 
+    type="text"
+    value= {textValue}
+    onChange={handleTextValue}
+    className='border-black border-2 outline-none'
+     />
+    <FaRegPlusSquare
+    role='button'
+    onClick={handleClick}
+    size={30}
+    style={{
+      color: 'blue'
+    }}
+    />
+    </section>
+
+    <ul>
+     {
+      tasks.map((task,index)=> {
+        return (
+        <section key={tasks.indexOf(task)} className='flex gap-5 mt-5 ml-28'>
+          <input 
+          type="checkbox" 
+          className=' w-8 h-8'
+           />
+        <li>{task}</li>
+        <FaTrashAlt
+        role='button'
+        style={{
           color: 'blue',
+          fontSize: "30px"
         }}
-        onClick = {handleClick}
+        onClick={()=> {
+          handleDelete(index)
+        }}
          />
-      </section>
-      {
-        role.length > 0 ?
-         <Task text = {role} /> : ''
+        </section>
+        )
+      })
+     }
+    </ul>
 
-      }
-   
-      
     </>
-  );
-};
+  )
+}
 
-export default InputBar;
+export default InputBar
