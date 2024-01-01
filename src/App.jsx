9440739625f16fc45c47c9c3805a7ec3a5d4c682
Nathan George <georgenathan010@gmail.com>
2024-01-01 11:32:21 +0300

    started a vite project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import InputBar from './components/InputBar'
+
+const App = () => {
+  return (
+    <div>
+
+      <InputBar/>
+      
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/InputBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InputBar = () => {
+  return (
+    <div>
+        heloo
+        <InputBar type="text" />
+    </div>
+  )
+}
+
+export default InputBar
